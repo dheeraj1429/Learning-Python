4d5a82e3bb914f3ef243033c9933496ec1a66cae
dheeraj1429 <dheerajsingh1429@gmail.com>
2023-04-02 00:10:15 -0700

    learning python

---
+++ day1/main.py
@@ -0,0 +1,37 @@
+# print in python
+# print('hello world!' + 'hello world')
+# input('what is your name?')
+# print("hello" + input('what is your name?'))
+# print('Hi' + input('What is your name?') + input('Where are you from?'))
+# print(len(input('What is your name?') + input('Where are you from?')))
+
+# variables in python
+# name = input('what is your name?')
+# print(name)
+
+# name = 'Dheeraj'
+# print(name)
+
+# we can conver this into this
+# print(len(input('What is your name?') + input('Where are you from?')))
+# name = input('what is your name?')
+# length = len(name)
+# print(length)
+
+# a = 10, b = 100, a = 100, b = 10
+# a = 10
+# b = 100
+
+# c = a
+# a = b
+# b = c
+
+# variables naming
+# userName = input('what is your name?')
+# user_name = input('what is your name')
+
+# day 1 project
+# print('Welcome the band name genrator')
+# city = input('What is your ciry name?\n')
+# pet = input('What is your pet name?\n')
+# print('your band name could be ' + city + " " + pet)
