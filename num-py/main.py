c3a73b2fce9ec71acd3fa0b99661c2c03a945d06
dheeraj8585 <dheeraj@metaversitytechnologies.com>
2023-10-30 15:58:12 +0530

    learning

---
+++ num-py/main.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+a1 = np.array([1, 2, 3])
+a2 = np.array((1, 2, 3))
+zero = np.zeros(10)
+ones = np.ones(10)
+random_float = np.random.random(10)
+a6 = np.linspace(0, 10, 100)
+a7 = np.arange(0, 10, .2)
+
+# print(a1 * 2)
+# print(a6 * 2)
+# print(a6 > 2)
+y = a7**2
+x = a7
+# print(a7 * y)
+
+
+# xpoints = np.array([0, 6])
+# ypoints = np.array([0, 250])
+
+
+# plt.plot(xpoints, ypoints)
+# plt.plot(a1, a1 ** 2)
+# plt.show()
+
+
+# plt.plot(x, y)
+# plt.hist(y)
+# plt.show()
+
+# print(np.sin(30))
+# print(np.exp(30))
